91ad86e7a9913fb89aa16fb943b01f11c013a393
Gabrielle Ford <gabrielledeva@gmail.com>
2022-01-26 20:09:11 -0500

    listing by id functional

--- src/components/listingById/ListingElements.tsx
+++ src/components/listingById/ListingElements.tsx
@@ -2,12 +2,25 @@ import styled from "styled-components";
 
 // ** Main Div ** //
 export const ListingContainer = styled.div`
-  
+  padding: 20vh 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  max-width: 2000px;
+  height: 100vh;
+  transition: all 0.3s ease-in-out;
 `
 
 // ** Wrapper ** //
 export const ListingWrapper = styled.div`
-  
+  width: 35vw;
+  display: grid;
+  align-items: center;
+  justify-content: center;
+  margin: auto;
+  border-radius: 7px;
+  padding: 30px 0 20px 0px;
+  background: #edf5e1;
 `
 
 // ** Listing ** //
@@ -20,10 +33,15 @@ export const ListingH1 = styled.h1`
   
 `
 
-// ** Listing Image ** //
-export const ListingImage = styled.img`
+// ** Listing Owner ** //
+export const ListingUser = styled.h3`
   
 `
+
+// ** Listing Image ** //
+// export const ListingImage = styled.img`
+  
+// `
 
 // ** Listing Description ** //
 export const ListingDescription = styled.p`
@@ -43,6 +61,21 @@ export const ListingTagContainer = styled.div`
 
 // ** Listing Tags ** //
 export const ListingTag = styled.img`
+  
+`
+
+// ** Listing Form ** //
+export const ListingForm = styled.form`
+  
+`
+
+// ** Listing Label ** //
+export const ListingLabel = styled.label`
+  
+`
+
+// ** Listing Input ** //
+export const ListingInput = styled.input`
   
 `
 
