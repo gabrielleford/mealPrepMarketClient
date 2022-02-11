3a00bb4c2e82c8a720a710f0adf50e3248799f38
Gabrielle Ford <gabrielledeva@gmail.com>
2022-02-11 00:04:13 -0500

    most of listing by id restyled

--- src/components/listingById/ListingById.tsx
+++ src/components/listingById/ListingById.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import APIURL from "../helpers/environments";
-import { Navigate } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import { AppProps } from "../../App";
 import { ButtonDiv, Delivery, EditDelete, GetStarted, ListingContainer, IndivListingDescription, ListingForm, IndivListingH1, IndivListingImg, ListingLabel, IndivListingPrice, IndivListingTag, IndivListingTagContainer, ListingUser, ListingWrapper, Pickup, QuantityOption, QuantitySelect, ListingUserDiv, PreppedBy, SubmitOrder, Para, QuantityLabel } from "./ListingElements";
 import ConfirmDelete from "../confirmDelete/ConfirmDelete";
+import { Badge, Button, Card, Center, Container, Grid, Group, Image, Paper, Select, Text, Title } from "@mantine/core";
 
 type ListingProps = {
   user: AppProps['user'],
@@ -78,7 +79,6 @@ class ListingById extends React.Component<ListingProps, ListingState> {
         ownerName: `${res.user.firstName} ${res.user.lastName}`
       })
       this.state._isMounted && this.props.setWhat('listing');
-      console.log(res.id);
     })
     .catch(error => console.log(error))
   }
@@ -157,69 +157,126 @@ class ListingById extends React.Component<ListingProps, ListingState> {
 
   render(): React.ReactNode {
       return (
-        <ListingContainer>
-          {this.props.dlt && 
-            <ConfirmDelete what={this.props.what} dlt={this.props.dlt} listingID={this.state.listingID} sessionToken={this.props.sessionToken} user={this.props.user} setDelete={this.props.setDelete} clearToken={this.props.clearToken} response={this.props.response} setResponse={this.props.setResponse} />
-          }
-          <ListingWrapper>
-            <IndivListingH1>{this.state.title}</IndivListingH1>
-            {this.state.ownerID === this.props.user.userId ? 
-              <ListingUserDiv>
-                <ListingUser to={`/profile/${this.state.ownerID}`}>
-                  {this.state.ownerName}
-                </ListingUser>
-              </ListingUserDiv> :
-              <ListingUserDiv>
-                <PreppedBy>Prepared by </PreppedBy>
-                <ListingUser to={`/profile/${this.state.ownerID}`}>
-                  {this.state.ownerName}
-                </ListingUser>
-              </ListingUserDiv>
-            }
-            <IndivListingImg listingEdit={this.props.listingEdit} src={this.state.image} alt={this.state.title}/>
-            <IndivListingDescription>{this.state.description}</IndivListingDescription>
-            <IndivListingTagContainer>
-              {/* <IndivListingTag /> */}
-            </IndivListingTagContainer>
-            <IndivListingPrice>${this.state.price} USD</IndivListingPrice>
-            {this.state.ownerID === this.props.user.userId ? 
-            <>
-              <ButtonDiv>
-                <EditDelete onClick={this.editListing}>Edit</EditDelete>
-                <EditDelete onClick={() => this.props.setDelete(true)}>Delete</EditDelete>
-              </ButtonDiv>
-            </> : 
-            localStorage.getItem('Authorization') ?
-            <ListingForm onSubmit={this.placeOrder}>
-              <QuantityLabel>Quantity</QuantityLabel>
-              <QuantitySelect name='quantity' onChange={this.handleChange}>
-                <QuantityOption value={1}>1</QuantityOption>
-                <QuantityOption value={2}>2</QuantityOption>
-                <QuantityOption value={3}>3</QuantityOption>
-                <QuantityOption value={4}>4</QuantityOption>
-              </QuantitySelect>
-              <ButtonDiv>
-                <Pickup onClick={this.setFulfillment}>Pickup</Pickup>
-                <Delivery onClick={this.setFulfillment}>Delivery</Delivery>
-              </ButtonDiv>
-              <SubmitOrder type="submit">Place Order</SubmitOrder>
-            </ListingForm> :
-            <>
-            <Para>Interested in ordering?</Para>
-            <GetStarted to='/login'>Get Started!</GetStarted>
-            </>
-          }
-          </ListingWrapper>
-          {this.props.response === 200 && this.state._isMounted ?
-          <Navigate to='/' replace={true} /> :
-          this.props.listingEdit ? 
-          <Navigate to={`/listing/edit/${this.state.listingID}`} replace={true}/> : 
-          this.state.responseCode === 201 ?
-          <Navigate to={`/orders/${this.props.user.userId}`} replace={true} /> : ''
-          }
-        </ListingContainer>
+        <Container size={700}>
+          <Card radius='lg' padding='sm' className="listingCard">
+              <Title className="listingTitle" align="center" order={1}>{this.state.title}</Title>
+              <Group spacing={5} position="center">
+                {this.state.ownerID === this.props.user.userId ? 
+                <Text size="lg" sx={{color: '#379683', fontFamily: 'Open-Sans, sans-serif'}} align="center" variant="link" component={Link} to={`/profile/${this.state.ownerID}`}>{this.state.ownerName}</Text> :
+                <>
+                <Text className="listingText" size="lg">Prepared by</Text>
+                <Text size="lg" sx={{color: '#379683', fontFamily: 'Open-Sans, sans-serif'}} variant="link" component={Link} to={`/profile/${this.state.ownerID}`}>{this.state.ownerName}</Text>
+                </>}
+              </Group>
+              <Center>
+                <Card.Section>
+                  <Image className="listingImg" radius={10} src={this.state.image} width={550} height={400} />
+                </Card.Section>
+              </Center>
+              <Center>
+              <Text mt={-7} className="description">{this.state.description}
+              <br/>
+                <Center>
+                  <Badge mt={7} variant='filled' color='green' radius='lg' size="xl">${this.state.price} USD</Badge>
+                </Center>
+              </Text>
+              </Center>
+              {this.props.user.userId === this.state.ownerID ?
+              <Group mt='md' position="center" spacing='xl'>
+                <Button className="darkButton" radius='md' size='xl' compact>Edit</Button>
+                <Button className="darkButton" radius='md' size='xl' compact>Delete</Button>
+              </Group> :
+              localStorage.getItem('Authorization') ?
+              <Grid>
+                <Grid.Col>
+                  <Select data={[
+                    {value: '1', label:'1'},
+                    {value: '2', label:'2'},
+                    {value: '3', label:'3'},
+                    {value: '4', label:'4'},
+                  ]} />
+                </Grid.Col>
+                <Grid.Col>
+                  <Group>
+                    <Button radius='md' size='xl' compact>Pickup</Button>
+                    <Button radius='md' size='xl' compact>Deliver</Button>
+                  </Group>
+                </Grid.Col>
+                <Grid.Col>
+                  <Button>Place Order</Button>
+                </Grid.Col>
+              </Grid> :
+              <Group direction="column" position="center" spacing={0}>
+                <Text align="center" className="listingText" mt={20} size='xl'>Interested in ordering?</Text>
+                <Text size='xl' sx={{color: '#379683', fontFamily: 'Open-Sans, sans-serif'}} variant='link' component={Link} to='/login'>Get started!</Text>
+              </Group>
+              }
+          </Card>
+        </Container>
       )
   }
 }
 
 export default ListingById;
+
+{/* <ListingContainer>
+{this.props.dlt && 
+  <ConfirmDelete what={this.props.what} dlt={this.props.dlt} listingID={this.state.listingID} sessionToken={this.props.sessionToken} user={this.props.user} setDelete={this.props.setDelete} clearToken={this.props.clearToken} response={this.props.response} setResponse={this.props.setResponse} />
+}
+<ListingWrapper>
+  <IndivListingH1>{this.state.title}</IndivListingH1>
+  {this.state.ownerID === this.props.user.userId ? 
+    <ListingUserDiv>
+      <ListingUser to={`/profile/${this.state.ownerID}`}>
+        {this.state.ownerName}
+      </ListingUser>
+    </ListingUserDiv> :
+    <ListingUserDiv>
+      <PreppedBy>Prepared by </PreppedBy>
+      <ListingUser to={`/profile/${this.state.ownerID}`}>
+        {this.state.ownerName}
+      </ListingUser>
+    </ListingUserDiv>
+  }
+  <IndivListingImg listingEdit={this.props.listingEdit} src={this.state.image} alt={this.state.title}/>
+  <IndivListingDescription>{this.state.description}</IndivListingDescription>
+  <IndivListingTagContainer>
+    {/* <IndivListingTag /> */}
+//   </IndivListingTagContainer>
+//   <IndivListingPrice>${this.state.price} USD</IndivListingPrice>
+//   {this.state.ownerID === this.props.user.userId ? 
+//   <>
+//     <ButtonDiv>
+//       <EditDelete onClick={this.editListing}>Edit</EditDelete>
+//       <EditDelete onClick={() => this.props.setDelete(true)}>Delete</EditDelete>
+//     </ButtonDiv>
+//   </> : 
+//   localStorage.getItem('Authorization') ?
+//   <ListingForm onSubmit={this.placeOrder}>
+//     <QuantityLabel>Quantity</QuantityLabel>
+//     <QuantitySelect name='quantity' onChange={this.handleChange}>
+//       <QuantityOption value={1}>1</QuantityOption>
+//       <QuantityOption value={2}>2</QuantityOption>
+//       <QuantityOption value={3}>3</QuantityOption>
+//       <QuantityOption value={4}>4</QuantityOption>
+//     </QuantitySelect>
+//     <ButtonDiv>
+//       <Pickup onClick={this.setFulfillment}>Pickup</Pickup>
+//       <Delivery onClick={this.setFulfillment}>Delivery</Delivery>
+//     </ButtonDiv>
+//     <SubmitOrder type="submit">Place Order</SubmitOrder>
+//   </ListingForm> :
+//   <>
+//   <Para>Interested in ordering?</Para>
+//   <GetStarted to='/login'>Get Started!</GetStarted>
+//   </>
+// }
+// </ListingWrapper>
+// {this.props.response === 200 && this.state._isMounted ?
+// <Navigate to='/' replace={true} /> :
+// this.props.listingEdit ? 
+// <Navigate to={`/listing/edit/${this.state.listingID}`} replace={true}/> : 
+// this.state.responseCode === 201 ?
+// <Navigate to={`/orders/${this.props.user.userId}`} replace={true} /> : ''
+// }
+// </ListingContainer> */}
